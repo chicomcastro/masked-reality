8807e590c092d41cc540f3d7b7a7189f49e261f7
Francisco Castro <Franciscommcastro@gmail.com>
2019-10-07 01:46:33 -0300

    Menu

--- Assets/CanvasManager.cs
+++ Assets/CanvasManager.cs
@@ -7,13 +7,25 @@ public class CanvasManager : MonoBehaviour
 {
     public static CanvasManager instance;
     public Text blocksCountText;
+    public Text titleText;
+    public GameObject inventoryPanel;
 
     private void Awake()
     {
         instance = this;
     }
 
+    private void Start()
+    {
+        inventoryPanel.gameObject.SetActive(false);
+    }
+
     public void SetBlockText(int newBlockQuant) {
         blocksCountText.text = newBlockQuant != 0 ? (newBlockQuant).ToString() : "NOTHING!";
+    }
+
+    public void DeactiveMenu() {
+        inventoryPanel.gameObject.SetActive(true);
+        titleText.gameObject.SetActive(false);
     }
 }
---
+++ Assets/MenuPortal.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuPortal : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other) {
+        CanvasManager.instance.DeactiveMenu();
+        LevelManager.instance.NextLevel();
+    }
+}
